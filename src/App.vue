2775d1bbf931ffcc473b85cedf2ba69e1ce50c87
95geunii <sdk7968@gmail.com>
2022-01-24 09:31:17 +0900

    update profiles

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav-bar">
+      <span class="nav-bar-contents" @click="$router.push('/')">Home</span>
+      <span class="nav-bar-contents" @click="$router.push('/about')">About</span>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+body {
+  margin: 0;
+  background-color: #262626;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.nav-bar {
+  padding: 30px 40px;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.nav-bar-contents {
+  color: #f1f1f1;
+  font-size: 20px;
+  font-weight: 800;
+  margin: 0 10px 0 10px;
+  border: none !important;
+  cursor: pointer;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-bar-contents:hover {
+  color: #3fccf7;
 }
 </style>
